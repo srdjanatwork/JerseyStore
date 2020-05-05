dc33c830b4e7283470c0a03967b822cf975f1b1f
Srdjan Seferovic <seferovic@work.co>
2020-05-05 14:34:40 +0200

    initial setup + material ui + react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
+import { RouteList } from './lib/routes';
+
+import Test from './views/Test';
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path={ RouteList.home }>
+            sasdasd
+          </Route>
+          <Route path={ RouteList.test }>
+            <Test />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
